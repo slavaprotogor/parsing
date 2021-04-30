871418a56d07dc084724eea43cd292d2636e2e69
Vyacheslav Zanin <zanin.vyacheslav@220-volt.ru>
2021-04-30 16:44:25 +0300

    first commit

---
+++ lesson3/migrations/drop_db.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from db import engine
+from models import Base
+
+
+Base.metadata.drop_all(engine)
